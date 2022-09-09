a65799e7ed17afab563090b732393ddeeed1a988
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-08 19:17:20 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 * Return: 0 If successful
 *
-* Description: This prints "with proper grammar, but the outcome is a piece of art,
+* Description: This prints "with proper grammar, but the outcome is a piece * of art,
 */
 
 int main(void)

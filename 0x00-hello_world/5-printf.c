e95cf2697b7ba0808c9155dbfacc0e06a32c529e
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-11 10:00:43 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
+
 /**
-* main - Entry point
-* Return: 0 If successful
-*
-* Description: This prints "with proper grammar, but the outcome is a piece * of art,
-*/
+ * main - Entry point
+ * Takes no argument
+ *
+ * Description: This prints "with proper grammar, but the outcome is a piece * of art,
+ * Return: Return to zero if successful
+ */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }

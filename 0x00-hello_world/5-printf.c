3ba5bb9b9a9ccbbf0f35775e45a430a74cb3c7ae
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-11 10:08:38 -0700

    hello

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,7 +7,6 @@
  * Description: This prints "with proper grammar, but the outcome is a piece * of art,
  * Return: Return to zero if successful
  */
-
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");

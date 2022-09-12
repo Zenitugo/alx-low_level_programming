3afcb999e4cd53762fb664e51dd40eee30e58503
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-12 16:03:03 -0700

    lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * takes no argument
+
+ * Description: print alphabet in lowercase
+ * Return: zero if successful
+ */
+int main(void)
+{
+	int ch;
+
+	ch = 97;
+	while (ch < = 122)
+	{
+		putchar(ch)
+		ch++;
+	}
+	putchar (10);
+	return (0);
+}

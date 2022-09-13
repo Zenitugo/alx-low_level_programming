c6f560d28856ea47a36adf76b02e55336bdeb885
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-12 18:41:49 -0700

    alpha

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,7 +12,6 @@ int main(void)
 	int ch;
 
 	(ch <= 122);
-
 	{
 		putchar(ch);
 		ch++;
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * takes no argument
+ *
+ *Descripton: lower case then uppercase
+ *Return: zero if successful
+ */
+int main(Void)
+{
+	int ch;
+
+	if ch = 97;
+		while (ch <= 122);
+
+		{
+			putchar(ch;)
+			ch++;
+		}
+
+	ch = 65
+		while (ch <=90)
+		{
+			putchar(ch);
+			ch++;
+		}
+	putchar(10);
+	return (0)
+}

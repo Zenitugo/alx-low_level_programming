afbecce22f1f27218400dc40307315a6418fbe62
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-12 21:45:20 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h.
+
+/**
+ * main - entry point
+ *  takes no argument
+ *
+ * Description: Print possible combinations of
+ * single digits numbers
+ * Return: zero if sucessful
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar(c);
+		if (c != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

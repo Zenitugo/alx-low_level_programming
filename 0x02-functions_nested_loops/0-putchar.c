a76d04fdc40c35c9c8ff17e1f4b3975479c66c27
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-13 16:20:56 -0700

    task one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * takes no argument
+ *
+ * Description: print putchar followed
+ * by a new line
+ * Return: zero if successful
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}

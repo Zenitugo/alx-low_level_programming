1c8acc30cfe803cdbee06f3c4b07966d1eb3d4ae
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-12 21:29:58 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - entry poin
+ * takes no argument
+ *
+ * Description: Print number of base 16 in lower case
+ * Return: zero if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n <= 57 ; n++)
+	{
+		putchar(ch);
+	}
+	for (n = 97 ; n <= 102 ; n++)
+	{
+		putchar(ch);
+	}
+	putchar(10);
+	return (0);
+}

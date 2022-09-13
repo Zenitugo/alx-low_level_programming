c76be42341c578ac94b640dffd3129fcf01f9ad0
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-12 17:09:42 -0700

    lowercase

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,12 +20,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	x = n % 10;
 	if (x > 5)
-		printf("Last digit of %d is %d and is greater that 5\n", n, x);
+		printf("Last digit of x is %d and is greater that 5\n", n, x);
 
 	else if (x == 0)
-		printf("Last digit of %d is %d && is zero\n", n, x);
+		printf("Last digit of x is %d and is 0\n", n, x);
 
 	else
-		printf("Last digit of %d is %d && is less than 6 && not zero\n", n, x);
+		printf("Last digit of x is %d and is less than 6 and not 0\n", n, x);
 	return (0);
 }

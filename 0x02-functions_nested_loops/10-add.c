c77e19be9f4f2278eb8c0e31bee4d7bd991bc0bc
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-14 07:00:41 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @m: first argument
+ * @n: second argument
+ * Description:  function that adds two integers and returns the result
+ * Return: value of the sum
+ */
+
+int add(int m,  int n)
+{
+	int sum;
+
+	sum = (m + n)
+
+	return (sum);
+}

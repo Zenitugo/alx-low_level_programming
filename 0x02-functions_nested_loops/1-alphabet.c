e8eaf56790aabf4f6c98db810e88e7ef6a66bf4b
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-13 17:22:05 -0700

    task one

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,16 +8,18 @@
  * lower case
  * Return: zero if successful
  */
-int main(void)
+void print_alphabet(void);
 {
 	char ch;
 
 	ch = 97;
-		while (ch <= 122)
-		{
-			_putchar(ch);
-			ch++;
-		}
+
+	while (ch <= 122)
+	{
+		_putchar(ch);
+		ch++;
+	}
 	_putchar(10);
 	return (0);
+}
 }

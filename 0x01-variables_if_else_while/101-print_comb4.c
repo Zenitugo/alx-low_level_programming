56e3062526ad584470d7953a8600a2f6000f90bf
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-14 14:39:01 -0700

    task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,10 +21,8 @@ int main(void)
 				if (m > p && p > i)
 				{
 					putchar(i + '0');
-					putchar((i % 10) + '0');
-					putchar(' ');
-					putchar((p / 10) + '0');
-					putchar((p % 10) + '0');
+					putchar(p + '0');
+					putchar(m + '0');
 					if (i != 7)
 					{
 						putchar(',');

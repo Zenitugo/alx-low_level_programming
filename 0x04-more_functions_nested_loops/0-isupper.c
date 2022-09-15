3a3634f3f72167b2952cd5160b021ab987256244
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-15 07:53:05 -0700

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ * @c: is uppercase
+ * Return: (1) if successful other wise return (0)
+ */
+
+int _isupper(int c);
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c _isupper(c));
+	c = 'a';
+	printf("%c: %d\n", c, _isupper(c));
+	return (0);
+}

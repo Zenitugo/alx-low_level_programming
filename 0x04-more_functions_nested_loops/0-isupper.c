a32b450b8f2c65862b3622f62d3015087e7c3742
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-16 15:09:36 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  *  _isupper - check the code.
@@ -8,13 +9,7 @@
 
 int _isupper(int c);
 {
-	int = c;
-
 	if (c >= 65 && c <= 90)
-		return (1);
-	
-	else
-	
-		return (0);
-	
+	return (1);
+	return (0);
 }

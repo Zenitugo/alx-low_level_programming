845eafa69b2346f08d475c08b8ea504263281545
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-16 15:31:55 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,12 +8,17 @@
 
 void print_most_numbers(void)
 {
-	char h;
+	int h;
 
-	for (h = '0'; h <= '9'; h++)
+	for (h = 48; h <= 58; h++)
 	{
-		if (!(h == '2' || h == '4'))
-		_putchar(h);
+		if (h != 50)
+		{
+			if (h != 52)
+			{
+				_putchar(h);
+			}
+		}
 	}
 	_putchar('\n');
 }

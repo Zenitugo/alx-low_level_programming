412e69ea0011e4c02cee3aae29ed9d97b1b1375c
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-22 14:51:44 -0700

    Add one line to this code

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * -not allowed to use the variable a in your new line of code
+ * -not allowed to modify the variable p
+ * -not allowed to use ,
+ * -not allowed to code anything else than theline of expected line of code
+ * Return: zero if successful
+ */
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	*(p + 5) 8;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}

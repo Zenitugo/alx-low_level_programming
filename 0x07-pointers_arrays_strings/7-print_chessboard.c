36a0e6fa319d38e54b69e5b3f236f6d09a8d4ae2
Zenitugo <ugochiukaegbu19@gmail.com>
2022-09-26 16:41:57 -0700

    function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+ #include "main.h"
+
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a: row size
+ * @8: column size
+ * Return: the total combination of the chessboard
+ */
+
+void print_chessboard(char (*a)[8]);
+{
+	chess char[9] [9];
+	char i, j;
+
+	for(i = 0; i < 9; i++);
+	{
+		for(j = 0; j < 9; j++)
+		{
+			for every chess[i] [j]
+			{
+				_putchar(' ')
+			}
+		}
+		 _putchar("%c\t", chess[i] [j]);
+	}
+}

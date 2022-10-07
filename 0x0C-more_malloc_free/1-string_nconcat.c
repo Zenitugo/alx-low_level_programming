b2a81dc8d18c3d7fa3f9c2857a823719da072705
Zenitugo <ugochiukaegbu19@gmail.com>
2022-10-06 21:09:26 -0700

    function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,13 +16,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
 
 	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
 		;
 
-	for (ls2 = 0; ls2[i] != '\0'; ls2++)
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
 		;
 
 	if (n > ls2)
@@ -36,10 +37,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 
 	for (i = 0; i < lsout; i++)
+	{
 		if (i < ls1)
 			sout[i] = s1[i];
 		else
 			sout[i] = s2[i - ls1];
+	}
 	sout[i] = '\0';
+
 	return (sout);
 }
